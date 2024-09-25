35ca9dc35bba023a25168dacbb5c3671d3d17c29
tanaaym7 <tanayt39@gmail.com>
2024-09-25 11:03:21 +0530

    feat: Added login sign-up pages

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
-import {DrizzleAdapter} from "@auth/drizzle-adapter"
-import {db} from "./db/drizzle"
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db/drizzle";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [],
-})
+  pages: {
+    signIn: "/sign-in",
+  },
+});
